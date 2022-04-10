92a8beba870534d18ec86fe20e100d028635514e
qrhassan <103136385+qrhassan@users.noreply.github.com>
2022-04-10 15:33:19 -0400

    updated

---
+++ src/applications/App.java
@@ -0,0 +1,19 @@
+package applications;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double personHeight = 1.67;
+		//double result=7*1.67;
+		double flagpoleHeight=2.0;
+		double totalHeight = (personHeight*7)+flagpoleHeight;
+		
+		//System.out.println("The height is " + result);
+		
+		System.out.println(totalHeight);
+		
+	}
+
+}
